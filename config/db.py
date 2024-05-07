7d68088654b0e9dc57ad81bd2be700cff35f9013
Gaston Oficina <genovesegaston@gmail.com>
2024-05-07 19:00:57 -0300

    actualizacion de archivo db.py

--- config/db.py
+++ config/db.py
@@ -1,3 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-create_engine()
+nombreBaseDeDatos= "clientesdb"
+engine = create_engine("mysql+pymsql://root:root@localhost:3306/{}".format(nombreBaseDeDatos))
+conn = engine.connect()
+
