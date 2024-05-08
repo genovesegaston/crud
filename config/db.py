35fa9747c7b60df6e852d030e69f65fbfaf7f581
Gaston Oficina <genovesegaston@gmail.com>
2024-05-08 16:04:01 -0300

    fin configuracion user.py

--- config/db.py
+++ config/db.py
@@ -2,5 +2,7 @@ from sqlalchemy import create_engine, MetaData
 
 nombreBaseDeDatos= "clientesdb"
 engine = create_engine("mysql+pymsql://root:root@localhost:3306/{}".format(nombreBaseDeDatos))
+
+meta = MetaData
 conn = engine.connect()
 
