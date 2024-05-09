2048406cc82d05ae5faa581bf472a7830074a084
genovesegaston <genovesegaston@gmail.com>
2024-05-08 22:57:36 -0300

    dejando prolijito el repo

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from routes.user import user
-
+from schemas.user import User
+from config.db import conn
+from sqlalchemy import select
 app = FastAPI()
 
 app.include_router(user)
 
-@app.get("/{nombre}")
-async def saludar(nombre:int):
-    return f'Hola',nombre
+@app.get("/user")
+async def get_user(user:User):
+   return conn.execute(select()).fetchall()
+     
+
+@app.post("/user")
+async def  crear_usuario():
+    pass
--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
 nombreBaseDeDatos= "clientesdb"
-engine = create_engine("mysql+pymysql://root:root@localhost:3306/{}".format(nombreBaseDeDatos))
+engine = create_engine("mysql+pymysql://root:@localhost:3306/{}".format(nombreBaseDeDatos))
 
 meta = MetaData()
 conn = engine.connect()
